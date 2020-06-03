33083930c08a3f54ac17a628acd83cd53ae844b3
dengyiqian@qq.com <a3221325>
2020-06-03 08:10:22 +0800

    Library创建

---
+++ common_library/src/main/java/org/dyq/common/widget/ShapeTextView.java
@@ -0,0 +1,21 @@
+package org.dyq.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class ShapeTextView extends AppCompatTextView {
+
+    public ShapeTextView(Context context) {
+        super(context);
+    }
+
+    public ShapeTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ShapeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
