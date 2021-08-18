a736450b8e7b1afdec6df279977d5fe1fdb059e5
道三清 <sr.ba1587@gmail.com>
2021-08-18 14:02:18 +0700

    code update

---
+++ custom_library/src/main/java/com/custom/base/BaseDialog.kt
@@ -0,0 +1,15 @@
+package com.custom.base
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import androidx.annotation.StyleRes
+
+/**
+ * 作者 : dao sanqing
+ * 描述 : description
+ * 日期 : 2021/8/18 11:53 上午
+ */
+class BaseDialog @JvmOverloads constructor(context: Context): Dialog(context,R.style.CustomDialog) {
+
+}
