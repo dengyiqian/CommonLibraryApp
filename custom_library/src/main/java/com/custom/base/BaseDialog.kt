e404b225db2b5812d258445f4cb6f377a4270825
道三清 <sr.ba1587@gmail.com>
2021-08-18 14:06:40 +0700

    code update

--- custom_library/src/main/java/com/custom/base/BaseDialog.kt
+++ custom_library/src/main/java/com/custom/base/BaseDialog.kt
@@ -3,6 +3,8 @@ package com.custom.base
 import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.annotation.StyleRes
 
 /**
@@ -11,5 +13,15 @@ import androidx.annotation.StyleRes
  * 日期 : 2021/8/18 11:53 上午
  */
 class BaseDialog @JvmOverloads constructor(context: Context): Dialog(context,R.style.CustomDialog) {
+
+    override fun onStart() {
+        super.onStart()
+        window?.windowManager?.let {
+            window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            val params = window!!.attributes
+            params!!.width = (it.defaultDisplay.width * 0.8).toInt()
+            window!!.attributes = params
+        }
+    }
 
 }
