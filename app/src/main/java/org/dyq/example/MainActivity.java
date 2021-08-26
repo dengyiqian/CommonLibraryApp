1c383bee6b663d5df60d28613a54f2fe27b1a5c3
道三清 <sr.ba1587@gmail.com>
2021-08-26 14:04:48 +0700

    code update

--- app/src/main/java/org/dyq/example/MainActivity.java
+++ app/src/main/java/org/dyq/example/MainActivity.java
@@ -1,16 +1,13 @@
 package org.dyq.example;
 
 import android.graphics.Color;
-import android.os.Bundle;
-import android.widget.TextView;
 
-import com.custom.base.BaseViewMolde;
+import com.custom.base.viewmodel.BaseViewMolde;
 import com.custom.base.ToolbarActivity;
 import com.custom.base.utils.AppUtils;
 import com.custom.base.utils.SpannUtils;
 
 import org.dyq.example.databinding.ActivityMainBinding;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
@@ -39,10 +36,11 @@ public class MainActivity extends ToolbarActivity<ActivityMainBinding, BaseViewM
 
     @Override
     protected void initView() {
-        SpannUtils.getBuilder(mBinding.textView)
+        SpannUtils.getBuilder(mBinding.contentTextView)
                 .append(Objects.requireNonNull(AppUtils.getVersionName()))
                 .append("\n我是附加第一个").setTextColor(Color.RED)
                 .append("\n我是附加第二个")
+                .instImage(R.drawable.vector_back,0,0,50)
                 .append("\n我是附加第三个").setTextColor(Color.BLUE).setBold().setTextSize(30)
                 .build();
     }
